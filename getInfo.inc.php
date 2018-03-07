9d5d6aec3a1a483f5a0c5da3d6299d6536b6b33b
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2018-03-07 10:10:05 +0800

    Codes for fetching info from database

--- getInfo.inc.php
+++ getInfo.inc.php
@@ -1,4 +1,20 @@
 <?php
-  include 'config.inc.php';
-  
+  header('Content-Type: application/json');
+  if(isset($_GET['type'])){
+    $type = $_GET['type'];
+    $sqlAmb = 'CALL POS_fetchAmbInfo()';
+    $sqlSt = 'CALL POS_fetchStallInfo()';
+    if($type == "ambulant"){
+      $result = fetchInfo($sqlAmb);
+    }else if($type == "stall"){
+      $result = fetchInfo($sqlSt);
+    }
+    echo json_encode($result);
+  }
+  function fetchInfo($BusType){
+    include 'config.inc.php';
+    $stmt = $conn->prepare($BusType);
+    $stmt->execute();
+    return $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 ?>
