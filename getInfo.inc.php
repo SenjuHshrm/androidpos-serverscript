213f817456e480c8ec3fd6a5325b8f89f831e19a
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2018-03-07 16:14:17 +0800

    CHANGE PC

--- getInfo.inc.php
+++ getInfo.inc.php
@@ -1,7 +1,7 @@
 <?php
-  header('Content-Type: application/json');
-  if(isset($_GET['type'])){
-    $type = $_GET['type'];
+  if(isset($_POST['type'])){
+    $xrRes = array();
+    $type = $_POST['type'];
     $sqlAmb = 'CALL POS_fetchAmbInfo()';
     $sqlSt = 'CALL POS_fetchStallInfo()';
     if($type == "ambulant"){
@@ -15,6 +15,10 @@
     include 'config.inc.php';
     $stmt = $conn->prepare($BusType);
     $stmt->execute();
-    return $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    while($res = $stmt->fetchAll(PDO::FETCH_ASSOC)){
+      $xrRes['Names'][] = $res;
+    }
+    echo json_encode($xrRes);
   }
 ?>
