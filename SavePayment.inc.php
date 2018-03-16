b803cbcefd0fc2a65c700a3ce55f3d9220e637d9
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2018-03-16 15:01:17 +0800

    include file format fix

--- SavePayment.inc.php
+++ SavePayment.inc.php
@@ -1,9 +1,9 @@
 <?php
-  include 'config.php';
+  include 'config.inc.php';
   if(isset($_POST['type']) && isset($_POST['data'])){
     $result='';
     if($_POST['type'] == 'stall'){
-      
+
     }else if($_POST['type'] == 'ambulant'){
 
     }
