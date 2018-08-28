3abb4f8e6cff71682694deefa55f2d78bfff507b
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2018-08-28 09:37:56 +0800

    Dynamic setup for connection; added ignored files

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,17 +1,29 @@
 <?php
 	class DBConnect {
 		public function dbConn(){
-			$servername = "192.168.143.217";
-			$username = "it_admin_pylon";
-			$password = "2014adamistrative2015";
-			$dbname = "market_db";
+			$config = $this->fetchSettings();
+			$servername = trim($config[0]);
+			$username = trim($config[1]);
+			$password = trim($config[2]);
+			$dbname = trim($config[3]);
 			try {
 			  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 			  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			} catch(PDOException $e) {
-			    die("OOPs something went wrong");
+			    //die("OOPs something went wrong");
+					echo($e);
 			}
 			return $conn;
+		}
+		private function fetchSettings() {
+			$settings = array();
+			$loc = $_SERVER['DOCUMENT_ROOT'].'/connection.txt';
+			$file = fopen($loc, 'r');
+			while(!feof($file)){
+				$settings[] = fgets($file);
+			}
+			fclose($file);
+			return $settings;
 		}
 	}
 ?>
