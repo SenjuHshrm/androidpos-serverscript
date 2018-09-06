8871ae7de4910243a6c85da75d31049864407511
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2018-09-06 17:15:14 +0800

    Added route and script for Ambulant registration

---
+++ RegAmbulant.inc.php
@@ -0,0 +1,16 @@
+<?php
+   header('Content-Type: application/json;charset=utf-8');
+   header('X-Requested-With: XMLHttpRequest');
+   require $_SERVER['DOCUMENT_ROOT'].'/tasks/RegAmb.inc.php';
+   $response_data = "";
+   $ProReg = new RegisterAmbulant;
+   $InputData = file_get_contents('php://input');
+   $JSONData = json_decode($InputData);
+   $FName = $JSONData->FirstName;
+   $MName = $JSONData->MiddleName;
+   $LName = $JSONData->LastName;
+   $loc = $JSONData->Location;
+   $locNum = $JSONData->LocationNumber;
+   $response_data = $ProReg->proc_reg($FName, $MName, $LName, $loc, $locNum);
+   echo $response_data;
+?>
---
+++ tasks/RegAmb.inc.php
@@ -0,0 +1,24 @@
+<?php
+ class RegisterAmbulant {
+    public function proc_reg($firstname, $middlename, $lastname, $location, $locationNum){
+      require_once $_SERVER['DOCUMENT_ROOT'].'/config/config.inc.php';
+      $CreateConn = new DBConnect;
+      $conn = $CreateConn->dbConn();
+      $res = "";
+      $sql = 'CALL POS_AddAmbulant(:firstname, :middlename, :lastname, :location, :locationNum)';
+      try {
+         $stmt = $conn->prepare($sql);
+         $stmt->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+         $stmt->bindParam(':middlename', $middlename, PDO::PARAM_STR);
+         $stmt->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+         $stmt->bindParam(':location', $location, PDO::PARAM_STR);
+         $stmt->bindParam(':locationNum', $locationNum, PDO::PARAM_STR);
+         $stmt->execute();
+         $res = "true";
+      } catch(Exception $e) {
+         $res = "false";
+      }
+      return $res;
+    }
+ }
+?>
